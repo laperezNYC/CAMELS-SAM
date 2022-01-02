774d786177ff5ac6bd98df5a01d51267055ab1d6
L.A. Perez <laperez5@asu.edu>
2022-01-02 17:01:50 -0500

    Update conf.py
    to have my info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CAMELS-SAM'
+copyright = '2022, Perez'
+author = 'Perez, L.A.'
 
 release = '0.1'
 version = '0.1.0'
